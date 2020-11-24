cd2be20272b66247ab0380a577f992c1b27b728a
murielfritsch <67455284+murielfritsch@users.noreply.github.com>
2020-11-24 12:39:23 +0200

    Update settings.py
    added the pythonanywhere as allowed_host

--- mydjangosite/settings.py
+++ mydjangosite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_hi1+6l+8^hkrm8t2^)znc2unc#6+#qj**)+1k264m^t&v$@cb'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mufr9002.pythonanywhere.com']
 
 
 # Application definition
